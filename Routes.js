b1a9a34b8d63f30e045177f0ccae07a62dccfb63
danieltavares1301 <daniel.tavares.1301@gmail.com>
2023-03-16 14:31:04 -0300

    refactor: componentiza HomeTabs

--- Routes.js
+++ Routes.js
@@ -9,55 +9,9 @@ import Scanner from './src/pages/camera';
 import Relatorios from './src/pages/relatorios';
 import DemoText from './src/pages/equipamentos/DemoText';
 
-import {Ionicons, AntDesign} from '@expo/vector-icons';
+import {HomeTabs} from './src/components/HomeTabs';
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-const HomeTabs = () => {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      shifting={false}
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: '#141414',
-        },
-      }}>
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        style
-        options={{
-          tabBarLabel: 'Home',
-          headerShown: false,
-          tabBarIcon: ({color}) => (
-            <Ionicons name="home" size={24} color="#fff" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Scanner"
-        component={Scanner}
-        options={{
-          tabBarLabel: 'Ler QR Code',
-          tabBarIcon: ({color}) => (
-            <AntDesign name="qrcode" size={24} color="#fff" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Relatorios"
-        component={Relatorios}
-        options={{
-          tabBarLabel: 'Relatorios',
-          tabBarIcon: ({color}) => (
-            <Ionicons name="md-newspaper" size={24} color="#fff" />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
 
 const Routes = () => {
   return (
---
+++ src/components/HomeTabs.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import Home from '../pages/home';
+import Scanner from '../pages/camera';
+import Relatorios from '../pages/relatorios';
+
+import {Ionicons, AntDesign} from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export const HomeTabs = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      shifting={false}
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#141414',
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        style
+        options={{
+          tabBarLabel: 'Home',
+          headerShown: false,
+          tabBarIcon: ({color}) => (
+            <Ionicons name="home" size={24} color="#fff" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Scanner"
+        component={Scanner}
+        options={{
+          tabBarLabel: 'Ler QR Code',
+          tabBarIcon: ({color}) => (
+            <AntDesign name="qrcode" size={24} color="#fff" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Relatorios"
+        component={Relatorios}
+        options={{
+          tabBarLabel: 'Relatorios',
+          tabBarIcon: ({color}) => (
+            <Ionicons name="md-newspaper" size={24} color="#fff" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
